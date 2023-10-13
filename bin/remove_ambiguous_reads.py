a89991721c0848dd1db3bc1ef93561af8b72ae9b
Christian Atallah <chrisata@ebi.ac.uk>
2023-10-13 16:32:32 +0100

    added single-end support to remove_ambiguous_reads script and nf process

--- bin/remove_ambiguous_reads.py
+++ bin/remove_ambiguous_reads.py
@@ -10,8 +10,8 @@ def parse_args():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-f", "--fwd", required=True, type=str, help="Path to forward fastq file")
-    parser.add_argument("-r", "--rev", required=True, type=str, help="Path to reverse fastq file")
+    parser.add_argument("-f", "--fwd", required=True, type=str, help="Path to forward (or single-end) fastq file")
+    parser.add_argument("-r", "--rev", required=False, type=str, help="Path to reverse fastq file")
     parser.add_argument("-s", "--sample", required=True, type=str, help="Sample ID")
     args = parser.parse_args()
     
@@ -25,31 +25,51 @@ def parse_args():
 def main():
 
     _FWD, _REV, _SAMPLE = parse_args()
+
+    fwd_handle = gzip.open(_FWD, "rt")
+    fwd_reads = SeqIO.to_dict(SeqIO.parse(fwd_handle, "fastq"))
+    fwd_handle.close()
+
+    paired_end = True
+
+    if _REV == None:
+        paired_end = False
+    else:
+        rev_handle = gzip.open(_REV, "rt")
+        rev_reads = SeqIO.to_dict(SeqIO.parse(rev_handle, "fastq"))
+        rev_handle.close()
     
     remove_lst = []
 
-    with gzip.open(_FWD, "rt") as fwd_handle, gzip.open(_REV, "rt") as rev_handle:
-        fwd_reads = SeqIO.to_dict(SeqIO.parse(fwd_handle, "fastq"))
-        rev_reads = SeqIO.to_dict(SeqIO.parse(rev_handle, "fastq"))
+    for read_id in fwd_reads.keys():
 
-        for read_id in fwd_reads.keys():
-
-            if "N" in str(fwd_reads[read_id].seq):
-                print(read_id)
-                remove_lst.append(read_id)
-                continue
-            elif "N" in str(rev_reads[read_id].seq):
-                print(read_id)
-                remove_lst.append(read_id)
-                continue
-
+        if "N" in str(fwd_reads[read_id].seq):
+            print(read_id)
+            remove_lst.append(read_id)
+            continue
+        elif paired_end and "N" in str(rev_reads[read_id].seq):
+            print(read_id)
+            remove_lst.append(read_id)
+            continue
 
     [ fwd_reads.pop(read_id) for read_id in remove_lst ]
-    [ rev_reads.pop(read_id) for read_id in remove_lst ]
-    
-    with bgzf.BgzfWriter(f"./{_SAMPLE}_noambig_1.fastq.gz", "wb") as fwd_handle, bgzf.BgzfWriter(f"./{_SAMPLE}_noambig_2.fastq.gz", "wb") as rev_handle:
+    if paired_end:
+        [ rev_reads.pop(read_id) for read_id in remove_lst ]
+
+    if paired_end:
+        
+        fwd_handle = bgzf.BgzfWriter(f"./{_SAMPLE}_noambig_1.fastq.gz", "wb")
+        rev_handle = bgzf.BgzfWriter(f"./{_SAMPLE}_noambig_2.fastq.gz", "wb")
+        
         SeqIO.write(sequences=fwd_reads.values(), handle=fwd_handle, format="fastq")
         SeqIO.write(sequences=rev_reads.values(), handle=rev_handle, format="fastq")
+
+        fwd_handle.close()
+        rev_handle.close()
+    else:
+        fwd_handle = bgzf.BgzfWriter(f"./{_SAMPLE}_noambig.fastq.gz", "wb")
+        SeqIO.write(sequences=fwd_reads.values(), handle=fwd_handle, format="fastq")
+        fwd_handle.close()
 
 if __name__ == "__main__":
     main()
